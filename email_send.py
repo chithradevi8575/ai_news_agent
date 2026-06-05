f9cb4e56ef0aa03fa3e84650a6d4832c43d80f18
chithradevi8575 <chithradevicse010@gmail.com>
2026-06-05 11:11:40 +0530

    Add files via upload

--- email_send.py
+++ email_send.py
@@ -45,8 +45,8 @@ def send_email(file_path, news):
 
     msg.attach(part)
 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    
 
     server.login(
         EMAIL_USER.strip(),
